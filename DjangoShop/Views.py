1fac901061fcf4e3487992f9299d4fc2fc1cf1c0
Zeynab Khalifeh <zeynabkhalifehh@gmail.com>
2023-12-19 19:58:16 +0330

    Add a form to About Us page.

--- DjangoShop/Views.py
+++ DjangoShop/Views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
 
 
 def home(request):
@@ -10,12 +11,18 @@ def home(request):
 
 def about_us(request):
     context = {
-        'about': 'Hi. This is about us page.'
+        'about_us': 'Hi. This is about us page.'
     }
     return render(request, 'about_us.html', context)
 
 def contact_us(request):
+    contact_form = ContactForm()
+    if request.method == "POST":
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('message'))
     context = {
-        'contact': 'Hi. This is contact us page.'
+        'contact_us': 'Hi. This is contact us page.',
+        'contact_form': contact_form
     }
     return render(request, 'contact_us.html', context)
